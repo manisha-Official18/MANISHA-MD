423391f3aac121e9237bf4061ab68872e96a3e25
manisha Sasmitha <manisha1718manisha@gmail.com>
2025-09-05 09:21:49 +0530

    MANISHA-MD➥

---
+++ config.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "NrtjHYpR#tt9Xfp8krywDjmlfVZ0GzkApCqlljm49YoTr2j5VrWg",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "94721551183",
+    MODE: process.env.MODE || "private",
+    PREFIX: process.env.PREFIX || ".",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
+    READ_MESSAGE: process.env.READ_MESSAGE || "false",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
+    AUTOLIKESTATUS: process.env.AUTOLIKESTATUS || "false",
+    AUTO_TYPING: process.env.AUTO_TYPING || "true",
+    AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+    ANTI_CALL: process.env.ANTI_CALL || "false",
+    BAD_NUMBER_BLOCKER: process.env.BAD_NUMBER_BLOCKER || "false",
+    UNIFIED_PROTECTION: process.env.UNIFIED_PROTECTION || "kick", // "off" | "warn" | "kick" | "strict"
+    };
+    
