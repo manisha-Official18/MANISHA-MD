7595a58d48d01280c61e51aba36c6d7478737c3f
manisha Sasmitha <manisha1718manisha@gmail.com>
2025-10-12 16:07:14 +0530

    MANISHA-MD➥

--- config.js
+++ config.js
@@ -6,22 +6,22 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "94721551183",
-    MODE: process.env.MODE || "private",
-    PREFIX: process.env.PREFIX || ".",
-    AUTO_REACT: process.env.AUTO_REACT || "false",
-    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
-    READ_MESSAGE: process.env.READ_MESSAGE || "false",
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
-    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
-    AUTOLIKESTATUS: process.env.AUTOLIKESTATUS || "false",
-    AUTO_TYPING: process.env.AUTO_TYPING || "true",
-    AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
-    ANTI_CALL: process.env.ANTI_CALL || "false",
-    BAD_NUMBER_BLOCKER: process.env.BAD_NUMBER_BLOCKER || "false",
-    UNIFIED_PROTECTION: process.env.UNIFIED_PROTECTION || "kick", // "off" | "warn" | "kick" | "strict"
+    SESSION_ID: process.env.SESSION_ID || "manaofc~", //(manaofc~ ) මෙහෙම තියන එක අයින් කරන්නෙ නැතුව සිසන් අයිඩ් එක දාන්න
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "94721551183", // whatsapp number enter
+    MODE: process.env.MODE || "private", // public / private / group / inbox
+    PREFIX: process.env.PREFIX || ".", // command prefix
+    AUTO_REACT: process.env.AUTO_REACT || "false", // true / false
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", // inbox / log
+    READ_MESSAGE: process.env.READ_MESSAGE || "false", // true / false
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", // true / false
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false", // true / false
+    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false", // true / false
+    AUTOLIKESTATUS: process.env.AUTOLIKESTATUS || "false", // true / false
+    AUTO_TYPING: process.env.AUTO_TYPING || "true", // true / false
+    AUTO_RECORDING: process.env.AUTO_RECORDING || "true", // true / false
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true", // true / false
+    ANTI_CALL: process.env.ANTI_CALL || "false", // true / false
+    BAD_NUMBER_BLOCKER: process.env.BAD_NUMBER_BLOCKER || "false", // true / false
+    UNIFIED_PROTECTION: process.env.UNIFIED_PROTECTION || "kick", // off / warn / kick / strict
     };
     
